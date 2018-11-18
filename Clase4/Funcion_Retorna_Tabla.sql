1782f490e33bc0bf9160aaf520eeeb6ff34a34f3
Gustavo Coronel <gcoronelc@gmail.com>
2018-11-18 07:22:56 -0500

    Clase 04

---
+++ Clase4/Funcion_Retorna_Tabla.sql
@@ -0,0 +1,40 @@
+-- ============================================
+-- Funcion que retorna una resultado
+-- ============================================
+
+Create Type scott.mytype As Object(
+  field1 Number,
+  field2 Varchar2(50)
+);
+
+Create Type mytypelist As Table Of mytype;
+
+Create Or Replace Function FN_GET_EMPS
+Return mytypelist Pipelined
+Is
+  v_mytype mytype;
+Begin
+  For r In (Select * From emp) Loop
+    v_mytype := mytype( r.empno, r.ename );
+    Pipe Row (v_mytype);
+  End Loop;
+  Return;
+End FN_GET_EMPS;
+
+Select * From Table(FN_GET_EMPS);
+
+
+Create Or Replace Function FN_GET_EMPS2
+( coddept Number )
+Return mytypelist Pipelined
+Is
+  v_mytype mytype;
+Begin
+     For r In (Select * From emp Where deptno = coddept) Loop
+         v_mytype := mytype( r.empno, r.ename );
+         Pipe Row (v_mytype);
+     End Loop;
+     Return;
+End FN_GET_EMPS2;
+
+Select * From Table(FN_GET_EMPS2(30));
